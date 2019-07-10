4aca9040a6f25bf05860a768ae9d7f6b3a034674
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-10 06:46:28 -0600

    Finished money change greedy algorithm problem

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,22 @@
 # Uses python3
 import sys
 
+# Let m be the input to change into coins
 def get_change(m):
-    #write your code here
-    return m
+    currentValue = m
+    numCoins = 0
+
+    while currentValue > 0:
+        if currentValue >= 10:
+            currentValue = currentValue - 10
+        elif currentValue < 10 and currentValue >= 5:
+            currentValue = currentValue - 5
+        elif currentValue < 5 and currentValue >= 1:
+            currentValue = currentValue - 1
+        numCoins = numCoins + 1
+
+    return numCoins
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
