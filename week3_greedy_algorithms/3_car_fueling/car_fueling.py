c73e0adef99f1a7d485fc44ffdda3872001a2207
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-10 07:12:24 -0600

    Starting car refuelling greedy problem

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -4,6 +4,24 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     # write your code here
+    numRefill = 0
+    currentRefill = 0
+
+    while currentRefill <= len(stops):
+        lastRefill = currentRefill
+
+        while(currentRefill <= len(stops) and (distance[currentRefill + 1] - distance[lastRefill] <= tank)):
+            currentRefill += 1
+
+        if currentRefill == lastRefill:
+            return -1
+
+        if currentRefill <= stops:
+            numRefill += 1
+
+    return numRefill
+
+
     return -1
 
 if __name__ == '__main__':
