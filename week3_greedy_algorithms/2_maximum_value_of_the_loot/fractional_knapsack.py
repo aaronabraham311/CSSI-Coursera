6c0a67ef86ed04382eea7e559d4f532382a1627e
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-10 06:58:10 -0600

    Finished maximum value of loot greedy problem

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,9 +1,32 @@
 # Uses python3
 import sys
 
+def value_per_weight (weights, values):
+    vpw = []
+
+    for i in len(weights):
+        vpw[i] = values[i]/weights[i]
+
+    vpw.sort(reverse = True)
+
+    return vpw
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
+    total_weight = capacity
+
     # write your code here
+    vpw = value_per_weight(weights, values)
+
+    for _ in len(values):
+        if capacity == 0:
+            return value
+        else:
+            for i in len(vpw):
+                min_weight = min(weights[i], total_weight)
+                value = value + min_weight * vpw[i]
+                total_weight = total_weight - min_weight
+
 
     return value
 
