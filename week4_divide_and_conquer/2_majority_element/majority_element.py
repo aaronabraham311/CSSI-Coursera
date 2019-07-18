4e01e466de8006f7251666dfae40ca94ae3f9b90
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-18 07:20:16 -0600

    Finished majority element exercise

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -2,11 +2,32 @@
 import sys
 
 def get_majority_element(a, left, right):
+
+    # Base cases (either array has size 1 or 2)
     if left == right:
         return -1
     if left + 1 == right:
         return a[left]
-    #write your code here
+
+    # Recursive calls to get majority element in left and right subarrays
+    left_majority_element = get_majority_element(a, left, ((left + right - 1) // 2) + 1)
+    right_majority_element = get_majority_element(a, ((left + right - 1) // 2) + 1, right)
+
+    # Finds out how many values in whole list correspond to right/left majority element
+    left_count = 0
+    right_count = 0
+    for i in range(left, right):
+        if a[i] == left_majority_element:
+            left_count += 1
+        elif a[i] == right_majority_element:
+            right_count += 1
+
+    # If there is more counts of the left_majority/right_majority element than half of the array length, then it must be in majority
+    if left_count > (right - left) // 2:
+        return left_majority_element
+    elif right_count > (right - left) // 2:
+        return right_majority_element
+
     return -1
 
 if __name__ == '__main__':
