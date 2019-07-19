22fe93f3a64ab75c059e0ad40911a6a5531a0442
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-19 07:45:15 -0600

    Finished lottery problem

--- week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -1,9 +1,38 @@
 # Uses python3
 import sys
+from itertools import chain
 
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
-    #write your code here
+
+    # Labelling each point with either l, r, or p if it is respectively left end, right end or point
+    starts = zip(starts, ['left'] * len(starts), range(len(starts)))
+    ends = zip(ends, ['right'] * len(ends), range(len(ends)))
+    points = zip(points, ['point'] * len(points), range(len(points)))
+
+    # Sorting all points on number line
+    sort_list = chain(starts, ends, points)
+    sort_list = sorted(sort_list)
+
+    segment_count = 0
+
+    '''
+    LOGIC:
+    If you reach a left endpoint, you have started a new segment. Increment the segment counter
+    If you reach a right endpoint, you have ended a segment, so decrement the segment counter
+    If you reach a point, enter the number of segments you are currently in
+    
+    The index only matters for points.
+    '''
+    for num, label, index in sort_list:
+        if label == 'left':
+            segment_count += 1
+        elif label == 'right':
+            segment_count -= 1
+        elif label == 'point':
+            cnt[index] = segment_count
+    return cnt
+
     return cnt
 
 def naive_count_segments(starts, ends, points):
@@ -23,6 +52,6 @@ if __name__ == '__main__':
     ends   = data[3:2 * n + 2:2]
     points = data[2 * n + 2:]
     #use fast_count_segments
-    cnt = naive_count_segments(starts, ends, points)
+    cnt = fast_count_segments(starts, ends, points)
     for x in cnt:
         print(x, end=' ')
