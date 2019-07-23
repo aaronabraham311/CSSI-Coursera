db37b4d562334b72e3b909e40be251decb772d0b
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-23 06:41:15 -0600

    Finished change dynamic programming problem

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -3,7 +3,7 @@ import sys
 
 def get_change(m):
     # Creating array to store previous optimal solutions to money changes
-    T = [100] * m
+    T = [None] * (m + 1)
     T[0] = 0
 
     # Loop through money:
@@ -14,11 +14,12 @@ def get_change(m):
         # Going through each coin and getting respective change
         # Remember basic change rule: change = (money - denomination) + 1 (+1 for change given)
         for denom in [1, 3, 4]:
-            change = T[(coin - denom) + 1]
+            if coin >= denom:
+                change = T[coin - denom] + 1
 
-            # If calculated change is less than the current solution, replaces it
-            if change < T[coin]:
-                T[coin] = change
+                # If calculated change is less than the current solution, replaces it
+                if change < T[coin]:
+                    T[coin] = change
 
     return T[len(T) - 1]
 
