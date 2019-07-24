d98cb172ae1cf17ac7b114e6b1801f82c5c3583a
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-24 07:07:16 -0600

    Finished primitive calculator DP problem

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -10,13 +10,11 @@ LOGIC
 def dp_optimal_sequence(n):
     # Creating array to store optimal solutions
     dp_array = [None] * (n + 1)
-    optimal_sequence = [None] * (n + 1)
+    optimal_sequence = [5] * (n + 1)
 
     # Base cases
     dp_array[0] = 0
     dp_array[1] = 0
-    optimal_sequence[0] = 0
-    optimal_sequence[1] = 0
 
     # Looping through all numbers from 1 to n
     for i in range(2, n + 1):
@@ -29,7 +27,6 @@ def dp_optimal_sequence(n):
                prev_optimal_solution = dp_array[i - 1] + 1
             elif j == 1: # Checking *2 move
                 if i % 2 == 0:
-                    print(i)
                     prev_optimal_solution = dp_array[i // 2] + 1
             else: # Checking *3 move
                 if i % 3 == 0:
@@ -39,20 +36,11 @@ def dp_optimal_sequence(n):
                 dp_array[i] = prev_optimal_solution
                 optimal_sequence[i] = j # Storing optimal move
 
-    return dp_array, optimal_sequence.reverse()
+    # Reversing list
+    optimal_sequence.reverse()
 
+    return dp_array, optimal_sequence
 
-def greedy_optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
 
 input = sys.stdin.read()
 n = int(input)
@@ -61,22 +49,37 @@ dp_array, optimal_sequence = dp_optimal_sequence(n)
 # Minimum number of operations
 print(dp_array[len(dp_array) - 1])
 
+
+
 # List of numbers leading to final number n
 num_list = []
 num = n
+newNum = 0
 
-for i in optimal_sequence:
+i = 0
+
+# LOGIC: Based on the operation saved in optimal_sequence, we will divide the orginal number down to zero
+# while saving intermediate numbers in num_list
+while True:
     num_list.append(num)
+    divisor = optimal_sequence[i]
 
-    if i  == 0:
-        num -= 1
-    elif i == 1:
-        num /= 2
+    if num == 1:
+        break
     else:
-        num /= 3
+        if divisor == 0:
+            newNum = num - 1
+        elif divisor == 1:
+            newNum = num / 2
+        else:
+            newNum = num / 3
+
+    difference = num - newNum
+    i = int(i + difference)
+    num = newNum
 
 # Printing out numbers
-num_list.reverse()
+num_list = reversed(num_list)
 
 for x in num_list:
-    print(x, end=' ')
+    print(int(x), end=' ')
