b40ee5f493e1c7736641b5e2372c56ef427af724
Aaron Abraham <aaronabraham311@gmail.com>
2019-07-25 06:44:11 -0600

    Finishing edit distance DP problem

--- week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -7,16 +7,16 @@ def edit_distance(s, t):
     d = []
 
     # Populating DP array
-    for _ in range(rows):
+    for _ in range(rows + 1):
         row = []
-        for _ in range(columns):
+        for _ in range(columns + 1):
             row.append(0)
         d.append(row)
 
-    for i in range(rows):
+    for i in range(rows + 1):
         d[i][0] = i
 
-    for i in range(columns):
+    for i in range(columns + 1):
         d[0][i] = i
 
     '''
@@ -26,8 +26,8 @@ def edit_distance(s, t):
     to a subsequence of a string (starting from the beginning)
     - By getting to the end of the DP array, we will need to use previous solutions for the number of edits
     '''
-    for j in range(columns):
-        for i in range(rows):
+    for j in range(columns + 1):
+        for i in range(rows + 1):
 
             # Calculating the number of edits to get to subsequence for each type of edit
             insertion = d[i][j - 1] + 1
@@ -36,13 +36,13 @@ def edit_distance(s, t):
             match = d[i - 1][j - 1]
 
             # Finding the optimal edit depending on whether last character of both strings match
-            if s[i] == t[j]:
+            if s[i - 1] == t[j - 1]:
                 d[i][j] = min(insertion, deletion, match)
             else:
                 d[i][j] = min(insertion, deletion, mismatch)
 
     # Returning very last entry of DP array
-    return d[rows - 1][columns - 1]
+    return d[rows][columns]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
